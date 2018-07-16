75b53e869c2facd6e3bebaf90aa20511fa715101
ChienTranJP <36807877+ChienTranJP@users.noreply.github.com>
2018-07-16 12:04:18 +0900

    fix miss logical error

--- public_html/js/input.js
+++ public_html/js/input.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded',function(){
     
     save.addEventListener('click',function (e) {
         e.preventDefault();
-        if(!subject.validity.valid || !memo.validity.validity){
+        if(subject.validity.valid === false || memo.validity.validity === false){
             alert('件名、メモはいずれも必須です');
             return;
         }
